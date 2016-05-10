364c058c71e8cbd6a05d84f089d6ac90d843a75f
Rajiv Bose <rbose85@users.noreply.github.com>
2016-05-10 07:52:11 +0100

    most basic working example of cycle.js app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-let elem = document.createElement('div');
-elem.id = 'output';
+import Cycle from '@cycle/core'
+import CycleDOM from '@cycle/dom'
+import Rx from 'rx'
 
 require('./index.css');
+
+const elem = document.createElement('div');
+elem.id = 'container';
+document.body.appendChild(elem);
+
+const main = () => ({
+  DOM: Rx.Observable.just(CycleDOM.h('span', 'Hi there!'))
+});
+
+const drivers = {
+  DOM: CycleDOM.makeDOMDriver('#container')
+};
+
+Cycle.run(main, drivers);
